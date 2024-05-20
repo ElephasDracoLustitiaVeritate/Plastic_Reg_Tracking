ad1480e8f672fac5e65c16a844249519a308deb2
ScipioElephasDracoLustitiaVeritate <105621999+ElephasDracoLustitiaVeritate@users.noreply.github.com>
2024-05-20 15:46:50 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
-import os
 
-DROPBOX_ACCESS_TOKEN = st.secrets["DROPBOX_ACCESS_TOKEN"]
-ARC_GIS_UNAME = st.secrets["ARC_GIS_UNAME"]
-ARC_GIS_PWORD = st.secrets["ARC_GIS_PWORD"]
+st.title("Plastic Regulations Map")
 
-st.write("Secrets loaded successfully.")
+try:
+    DROPBOX_ACCESS_TOKEN = st.secrets["DROPBOX_ACCESS_TOKEN"]
+    ARC_GIS_UNAME = st.secrets["ARC_GIS_UNAME"]
+    ARC_GIS_PWORD = st.secrets["ARC_GIS_PWORD"]
+    st.write("Secrets loaded successfully.")
+except KeyError as e:
+    st.write(f"KeyError: {e}")
+
+st.write("Available secrets:", st.secrets)
